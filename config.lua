4c7b20102e4557a8a35fc2ef2a03582683cdd595
Hakko <67168651+Hakkodevelopment@users.noreply.github.com>
2023-10-27 15:48:53 -0600

    Me, Mer, DV, DAV

--- config.lua
+++ config.lua
@@ -1,5 +1,6 @@
 config = {}
 
+
 config.framework = 'ND' -- ND, QB, ESX, or none
 
 config.EnableDV = true -- true/false. Enable or disable the delete vehicle command.
@@ -15,56 +16,56 @@ config.EnableMe = true -- true/false. Enable or disable the /me command
 config.MeCommand = 'me' -- The me command without the slash. Default: me
 config.MeLocation = 'head' -- head, chat, or both. Where to show the /me command.
 
-config.EnableMer = true, -- true/false. Enable or disable the /mer command.
+config.EnableMer = true -- true/false. Enable or disable the /mer command.
 config.MeRCommand = 'mer' -- The mer command without the slash. Default: mer
 config.MerLocation = 'head' -- head, chat, or both. Where to show the /mer command.
 
 config.EnableGme = true -- true/false. Enable or disable the global me command
 config.GMeCommand = 'gme' -- The gme command without the slash. Default: gme
 
-config.EnableGmer = true, -- true/false. Enable or disable the global mer command.
+config.EnableGmer = true -- true/false. Enable or disable the global mer command.
 config.GmerCommand = 'gmer' -- The gmer command without the slash. Default: gmer
 
-config.EnableDo = true, -- true/false. Enable or disable the local do command.
+config.EnableDo = true -- true/false. Enable or disable the local do command.
 config.DoCommand = 'do' -- The do command without the slash. Default: do
 
-config.EnableOOC = true, -- true/false. Enable or disable the local ooc command.
+config.EnableOOC = true -- true/false. Enable or disable the local ooc command.
 config.OOCCommand = 'ooc' -- The ooc command without the slash. Default: ooc
 
-config.EnableGOOC = true, -- true/false. Enable or disable the global ooc command.
+config.EnableGOOC = true -- true/false. Enable or disable the global ooc command.
 config.GOOCCommand = 'gooc' -- The global gooc command without the slash. Default: gooc
 
-config.EnableTwt = true, -- true/false. Enable or disable the tweet command.
+config.EnableTwt = true -- true/false. Enable or disable the tweet command.
 config.TwtCommand = 'tweet' -- The tweet command without the slash. Default: tweet
 
-config.EnableDarkWeb = true, -- true/false. Enable or disable the darkweb command.
+config.EnableDarkWeb = true -- true/false. Enable or disable the darkweb command.
 config.DarkWebCommand = 'darkweb' -- The darkweb command without the slash. Default: darkweb
 
-config.Enable911 = true, -- true/false. Enable or disable the 911 command.
-config.911Command = '911' -- The 911 command without the slash. Default: 911
+config.Enable911 = true -- true/false. Enable or disable the 911 command.
+config.NineOneOneCommand = '911' -- The 911 command without the slash. Default: 911
 
-config.EnableRT = true, -- true/false. Enable or disable the radio traffic command.
+config.EnableRT = true -- true/false. Enable or disable the radio traffic command.
 config.RTCommand = 'rt' -- The radio traffic command without the slash. Default: rt
 
-config.EnableNews = true, -- true/false. Enable or disable the news command.
+config.EnableNews = true -- true/false. Enable or disable the news command.
 config.NewsCommand = 'news' -- The news command without the slash. Default: news
 
-config.EnableAd = true, -- true/false. Enable or disable the ad command.
+config.EnableAd = true -- true/false. Enable or disable the ad command.
 config.AdCommand = 'ad' -- The ad command without the slash. Default: ad
 
-config.EnableAnnounce = true, -- true/false. Enable or disable the Announce command.
+config.EnableAnnounce = true -- true/false. Enable or disable the Announce command.
 config.AnnounceCommand = 'announce' -- The Announce command without the slash. Default: Announce
 config.AnnounceRestricted = true -- true/false. Whether or not the command should be restricted to any group with the permission command.Name
 
-config.EnableDiscord = true, -- true/false. Enable or disable the Discord command.
+config.EnableDiscord = true -- true/false. Enable or disable the Discord command.
 config.DiscordCommand = 'discord' -- The Discord command without the slash. Default: discord
 config.DiscordLink = 'discord.gg/perimeter' -- The discord link to display
 
-config.EnableTeamSpeak = true, -- true/false. Enable or disable the TeamSpeak command.
+config.EnableTeamSpeak = true -- true/false. Enable or disable the TeamSpeak command.
 config.TeamSpeakCommand = 'teamspeak' -- The TeamSpeak command without the slash. Default: teamspeak
 config.TeamSpeakLink = 'ts.example.com' -- The TeamSpeak link to display
 
-config.EnableWeb = true, -- true/false. Enable or disable the website command.
+config.EnableWeb = true -- true/false. Enable or disable the website command.
 config.WebCommand = 'website' -- The website command without the slash. Default: website
 config.WebLink = 'https://perimetergaming.com' -- The website link to display
 
--- src/client/main.lua
+++ src/client/main.lua
@@ -1,3 +1,212 @@
+local oxLib = GetResourceState("ox_lib")
+local name = nil
+
+if config.framework = 'ND' then
+    local NDCore = exports["ND_Core"]:GetCoreObject()
+    local char = NDCore.Functions.GetSelectedCharacter()
+
+    if char then 
+        name = chracter.firstName .. " " .. character.lastName
+    else
+        print('no character')
+    end
+elseif config.framework = 'QB' then
+    local QBCore = exports['qb-core']:GetCoreObject()
+    local Player = QBCore.Functions.GetPlayerData()
+    print(Player.CharInfo)
+elseif config.framework = 'ESX' then
+    
+else
+    name = GetPlayerName(PlayerPedId())
+end
+
+
+-- register commands
+if config.enableMe then
+    RegisterCommand(config.MeCommand, function(source, args, rawCommand) 
+        if config.MeLocation == 'head' or config.MeLocation == 'both' then
+            local text = "* " .. table.concat(args, " ") .. " *"
+            local ply = pedId()
+            local name = GetPlayerName(PlayerPedId())
+
+            if ply ~= -1 then
+                displayText(ply, text)
+            end
+
+            TriggerClientEvent("tam:toolBox:sendProximityMeMessage", -1, source, name, table.concat(args, " "))
+        elseif config.MeLocation == 'chat' or config.MeLocation == 'both'
+            local name = GetPlayerName(source)
+            TriggerClientEvent("tam:toolBox:sendProximityMeMessage", -1, source, name, table.concat(args, " "))
+        else
+            Citizen.Trace('You have an error in your tam_chattoolbox config.')
+        end
+    end)
+end
+
+if config.enableMer then 
+    RegisterCommand(config.MerCommand, function(source, args, rawCommand) 
+        if config.MerLocation == 'head' or config.MerLocation == 'both' then
+            local text = "* ^8" .. table.concat(args, " ") .. " *"
+            local ply = pedId()
+            local name = GetPlayerName(PlayerPedId())
+
+            if ply ~= -1 then
+                displayText(ply, text)
+            end
+
+            TriggerClientEvent("tam:toolBox:sendProximityMeMessage", -1, source, name, '^8' .. table.concat(args, " "))
+        elseif config.MerLocation == 'chat' or config.MerLocation == 'both'
+            local name = GetPlayerName(PlayerPedId())
+            TriggerClientEvent("tam:toolBox:sendProximityMeMessage", -1, source, name, '^8' .. table.concat(args, " "))
+        else
+            Citizen.Trace('You have an error in your tam_chattoolbox config.')
+        end
+    end)
+end
+
+if config.enableDV then 
+    RegisterCommand(config.DVCommand, function()
+        TriggerEvent("tam:chatToolbox:deleteVehicle")
+    end, config.DVRestricted)
+
+    TriggerEvent("chat:addSuggestion", config.DVCommand, "Deletes the vehicle you're sat in, or standing next to.")
+end
+RegisterCommand(config.DVACommand, function(source, args, rawCommand) 
+    TriggerClientEvent("tam:chatToolbox:delallveh", -1) 
+end, config.DAVRestricted)
+-- events
+RegisterNetEvent('tam:toolBox:sendProximityMeMessage', function(id, name, msg) 
+    local pedId = PlayerId()
+    local srvId = GetPlayerFromServerId(id)
+    if pedId == srvId then
+        TriggerEvent('chatMessage', "", {255, 0, 0}, " ^6 ^*ME | ^7" .. name .."^7: ".."^r " .. msg)
+    elseif GetDistanceBetweenCoords(GetEntityCoords(GetPlayerPed(srvId)), GetEntityCoords(GetPlayerPed(pedId)), true) < 19.999 then
+        TriggerEvent('chatMessage', "", {255, 0, 0}, " ^6 ^*ME | ^7" .. name .."^7: ".."^r " .. msg)
+    end
+end)
+
+RegisterNetEvent('tam:toolBox:sendProximityDoMessage', function(id, name, msg) 
+    local pedId = PlayerId()
+    local srvId = GetPlayerFromServerId(id)
+    if pedId == srvId then
+        TriggerEvent('chatMessage', "", {255, 0, 0}, " ^6 ^*ME | ^7" .. name .."^7: ".."^r " .. msg)
+    elseif GetDistanceBetweenCoords(GetEntityCoords(GetPlayerPed(srvId)), GetEntityCoords(GetPlayerPed(pedId)), true) < 19.999 then
+        TriggerEvent('chatMessage', "", {255, 0, 0}, " ^6 ^*ME | ^7" .. name .."^7: ".."^r " .. msg)
+    end
+end)
+
+RegisterNetEvent("tam:chatToolbox:deleteVehicle" function()
+    local ped = PlayerPedId()
+
+    if (DoesEntityExist(ped) and not IsEntityDead(ped)) then
+        local pos = GetEntityCoords(ped)
+        if (IsPedSittingInAnyVehicle(ped)) then
+            local veh = GetVehiclePedIsIn(ped, false)
+            if (GetPedInVehicleSeat(veh, -1) == ped) then
+                DeleteGivenVehicle(veh, numRetries)
+            else
+                if oxlib == 'started' then
+                    lib.notify({description = 'You must be in the driver\'s seat!', type = 'error'})
+                else
+                    Notify('~r~You must be in the driver\'s seat!')
+                end
+                
+            end
+        else
+            local inFrontOfPlayer = GetOffsetFromEntityInWorldCoords(ped, 0.0, distanceToCheck, 0.0)
+            local veh = GetVehicleInDirection(ped, pos, inFrontOfPlayer)
+            if (DoesEntityExist(veh)) then
+                DeleteGivenVehicle(veh, numRetries)
+            else
+                if oxlib == 'started' then
+                    lib.notify({description = 'You must be in or near a vehicle to delete it.', type = 'error'})
+                else
+                    Notify('~r~Failed to delete vehicle, trying again...')
+                end
+            end
+        end
+    end
+end)
+
+RegisterNetEvent("tam:chatToolbox:delallveh" function ()
+    for veh in EnumerateVehicles() do
+        if (not IsPedAPlayer(GetPedInVehicleSeat(veh, -1))) then 
+            SetVehicleHasBeenOwnedByPlayer(veh, false) 
+            SetEntityAsMissionEntity(veh, false, false) 
+            DeleteVehicle(veh)
+            if (DoesEntityExist(veh)) then
+                DeleteVehicle(veh)
+                if oxlib == 'started' then
+                    lib.notify({description = 'Deleted All Vehicles', type = 'Success'})
+                else
+                    Notify('Deleted All Vehicles!')
+                end
+            end
+        end
+    end
+end)
+
+-- functions
+local distanceToCheck = 5.0
+local numRetries = 5
+
+function DeleteGivenVehicle(veh, tMax)
+    local timeout = 0
+
+    SetEntityAsMissionEntity(veh, true, true)
+    DeleteVehicle(veh)
+    if (DoesEntityExist(veh)) then
+        if oxlib == 'started' then
+            lib.notify({description = 'Failed to delete vehicle, trying again...', type = 'error'})
+        else
+            Notify('~r~Failed to delete vehicle, trying again...')
+        end
+        while (DoesEntityExist(veh) and timeout < tMax) do
+            DeleteVehicle(veh)
+            if (not DoesEntityExist(veh)) then
+                if oxlib == 'started' then
+                    lib.notify({description = 'Vehicle Deleted', type = 'success'})
+                else
+                    Notify('Vehicle Deleted!')
+                end
+                
+            end
+            timeout = timeout + 1
+            Wait(500)
+            if (DoesEntityExist(veh) and (timeout == tMax - 1)) then
+                if oxlib == 'started' then
+                    lib.notify({description = 'Failed to delete vehicle after ' .. tMax .. ' retries.', type = 'error'})
+                else
+                    Notify('~r~Failed to delete vehicle after ' .. tMax .. ' retries.')
+                end
+            end
+        end
+    else
+        if oxlib == 'started' then
+            lib.notify({description = 'Vehicle Deleted', type = 'success'})
+        else
+            Notify('Vehicle Deleted!')
+        end
+    end
+end
+
+-- Gets a vehicle in a certain direction
+-- Credit to Konijima
+function GetVehicleInDirection(entFrom, coordFrom, coordTo)
+    local ray = StartShapeTestCapsule(coordFrom.x, coordFrom.y, coordFrom.z, coordTo.x, coordTo.y, coordTo.z, 5.0, 10, entFrom, 7)
+    local _, _, _, _, veh = GetShapeTestResult(ray)
+
+    if (IsEntityAVehicle(veh)) then
+        return veh
+    end
+end
+
+function Notify(text)
+    SetNotificationTextEntry("STRING")
+    AddTextComponentString(text)
+    DrawNotification(false, false)
+end
+
 local peds = {}
 local GetGameTimer = GetGameTimer
 
@@ -51,18 +260,56 @@ function displayText(ped, text)
     end
 end
 
-
-RegisterCommand(config.MeCommand, function() 
-    if config.MeLocation == 'head' or config.MeLocation == 'both' then
-        local text = "* " .. table.concat(args, " ") .. " *"
-        local ply = pedId()
-
-        if ply ~= -1 then
-            displayText(ply, text)
-        end
-    elseif config.MeLocation == 'chat' or config.MeLocation == 'both'
-        
-    else
-        Citizen.Trace('You have an error in your tam_chattoolbox config.')
+local entityEnumerator = {
+    __gc = function(enum)
+    if enum.destructor and enum.handle then
+        enum.destructor(enum.handle)
     end
-end)
+    enum.destructor = nil
+    enum.handle = nil
+    end
+}
+
+local function EnumerateEntities(initFunc, moveFunc, disposeFunc)
+    return coroutine.wrap(function()
+    local iter, id = initFunc()
+    if not id or id == 0 then
+        disposeFunc(iter)
+        return
+    end
+    
+    local enum = {handle = iter, destructor = disposeFunc}
+    setmetatable(enum, entityEnumerator)
+    
+    local next = true
+    repeat
+        coroutine.yield(id)
+        next, id = moveFunc(iter)
+    until not next
+    
+    enum.destructor, enum.handle = nil, nil
+    disposeFunc(iter)
+    end)
+end
+
+function EnumerateObjects()
+    return EnumerateEntities(FindFirstObject, FindNextObject, EndFindObject)
+end
+
+function EnumeratePeds()
+    return EnumerateEntities(FindFirstPed, FindNextPed, EndFindPed)
+end
+
+function EnumerateVehicles()
+    return EnumerateEntities(FindFirstVehicle, FindNextVehicle, EndFindVehicle)
+end
+
+function EnumeratePickups()
+    return EnumerateEntities(FindFirstPickup, FindNextPickup, EndFindPickup)
+end
+
+--[[Usage:
+for ped in EnumeratePeds() do
+    <do something with 'ped'>
+end
+]]
